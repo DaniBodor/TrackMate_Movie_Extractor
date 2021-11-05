0f626f0bef839fe30a1223d797a20f2aae37b6bf
DaniBodor <tatu.dani@gmail.com>
2021-11-05 15:51:17 +0100

    initial commit

---
+++ getTrackPositions.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 15:31:32 2021
+
+@author: dani
+"""
+
+
+
+
+
+import os
+import numpt as np
+import pandas as pd
+
+
+
+csvFile = './Spots.csv'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
